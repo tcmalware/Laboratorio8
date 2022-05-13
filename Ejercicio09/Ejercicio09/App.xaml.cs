c6810b2423de6a87c1e3836f0a7cd2e66d22c2a5
tcmalware <genarodvo@gmail.com>
2022-05-12 20:23:35 -0500

    Agregar archivos de proyecto.

---
+++ Ejercicio09/Ejercicio09/App.xaml.cs
@@ -0,0 +1,29 @@
+﻿using Laboratorio08;
+using System;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace Ejercicio09
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new NavigationPage(new MainPage());
+        }
+
+        protected override void OnStart()
+        {
+        }
+
+        protected override void OnSleep()
+        {
+        }
+
+        protected override void OnResume()
+        {
+        }
+    }
+}
